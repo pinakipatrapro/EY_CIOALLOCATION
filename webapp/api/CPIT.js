b8ca196bc62ea774303c1efa2f16c65d148950ab
pinakipatrapro <pinaki.patra.pro@gmail.com>
2018-08-31 02:27:02 +0000

    Issue with Aggregating Parent Node

--- webapp/api/CPIT.js
+++ webapp/api/CPIT.js
@@ -1,42 +1,184 @@
 sap.ui.define([
-    "sap/ui/base/Object"
+	"sap/ui/base/Object"
 ], function (BaseObject) {
-    "use strict";
+	"use strict";
 
-    var dataModel = function (model) {
-        this._data = []; // structure to hold the nested data
+	var dataModel = function (model) {
+		this._data = []; // structure to hold the nested data
 
-        this._model = model;
-        this._costPToCostCPath = '/ITServiceMaster';
-        this._costPToCostCData = [];
-        this._ITServicePath = '/CCVCP_CC';
-        this._ITServiceData = [];
-        this._endpoint = 'https://fg8b51648f83.us1.hana.ondemand.com/CIO/Allocation/Services/Allocation.xsodata'//'/eyhcp/CIO/Allocation/Services/Allocation.xsodata';
+		this._model = model;
+		this._costPToCostCPath = '/CCVCP_CC	';
+		this._costPToCostCData = [];
 
-        this.loadData = function () {
-            this._costPToCostCData  = this._fetchData(this._costPToCostCPath);
-            this._ITServiceData     = this._fetchData(this._ITServicePath);
-        };
+		this._ITServiceTower = '/ITServiceMaster?$select=ITTower';
+		this._ITServiceTowerData = [];
+		this._ITServiceSubTower = '/ITServiceMaster?$select=ITTower,ITSubTower';
+		this._ITServiceSubTowerData = [];
+		this._ITService = '/ITServiceMaster?$select=ITSubTower,ITService,ITServiceID';
+		this._ITServiceData = [];
 
-        //Trigger AJAX to fetch data
-        this._fetchData = function (path) {
-            return new Promise(function (resolve, reject) {
-                $.ajax({
-                    url: this._endpoint + path,
-                    method: "GET",
-                    success: function (response) {
-                        resolve(response);
-                    },
-                    error: function (error) {
-                        reject();
-                    }
-                });
-            }.bind(this));
-        };
-    };
+		this._endpoint = '/eyhcp/CIO/Allocation/Services/Allocation.xsodata';
 
-    var dataUploader = BaseObject.extend("pinaki.ey.CIO.allocation.api.CPIT", {
-        constructor: dataModel
-    });
-    return dataModel;
+		this.loadInitialData = function () {
+			return new Promise(function (res, rej) {
+				var dataLoadCompleted = new Promise(function (resolve, reject) {
+					Promise.all([
+						this._fetchData(this._costPToCostCPath),
+						this._fetchData(this._ITServiceTower),
+						this._fetchData(this._ITServiceSubTower),
+						this._fetchData(this._ITService)
+					]).then(function (value) {
+						this._costPToCostCData = value[0];
+						this._ITServiceTowerData = value[1];
+						this._ITServiceSubTowerData = value[2];
+						this._ITServiceData = value[3];
+						resolve();
+					}.bind(this));
+				}.bind(this));
+				dataLoadCompleted.then(function () {
+					var ITST2ITS = this._buildITST2ITSHierarchy();
+					var ITT2ITS = this._buildITT2ITSHierarchy(ITST2ITS);
+
+					var cpData = this._buildCPHierarchy();
+					var finalData = this._buildCCHierarchy(cpData, ITT2ITS);
+					res(finalData);
+				}.bind(this));
+			}.bind(this));
+
+		};
+
+		//Trigger AJAX to fetch data
+		this._fetchData = function (path) {
+			return new Promise(function (resolve, reject) {
+				if (path.indexOf('?') > -1) {
+					var jsonFormatText = '&$format=json';
+				} else {
+					jsonFormatText = '?$format=json';
+				}
+				$.ajax({
+					url: this._endpoint + path + jsonFormatText,
+					method: "GET",
+					success: function (response) {
+						resolve(response.d.results);
+					},
+					error: function (error) {
+						reject();
+					}
+				});
+			}.bind(this));
+		};
+
+		//Build Hierarchies
+		this._buildCPHierarchy = function () {
+			var dataOut = [];
+			var data = this._costPToCostCData;
+			data.forEach(function (e, i) {
+				var alreadyExists = false;
+				dataOut.forEach(function (f) {
+					if (e["CostPoolID"] === f["id"]) {
+						alreadyExists = true;
+					}
+				}.bind(this));
+				if (!alreadyExists) {
+					dataOut.push({
+						"level": "Cost Pool",
+						"name": e["CostPoolName"],
+						"value": 0,
+						"root": true,
+						"id": e["CostPoolID"],
+						"leaf": false,
+						"childSum": 0,
+						"nodeType": "display"
+					});
+				}
+			});
+			return dataOut;
+		};
+		this._buildCCHierarchy = function (CPData, ITT2ITS) {
+			var flatData = this._costPToCostCData;
+			CPData.forEach(function (e) {
+				e.child = [];
+				flatData.forEach(function (f) {
+					if (e["id"] === f["CostPoolID"]) {
+						e.child.push({
+							"level": "Cost Center",
+							"name": f["CostCenterName"],
+							"root": false,
+							"leaf": false,
+							"id": f["CostCenterID"],
+							"nodeType": "display",
+							"value": parseFloat(f["AmountFormatted"]),
+							"childSum": 0,
+							"child": JSON.parse(JSON.stringify(ITT2ITS))
+						});
+						e.value = e.value + parseFloat(f["AmountFormatted"]);
+					}
+				}.bind(this));
+			}.bind(this));
+			return CPData;
+		};
+		this._buildITST2ITSHierarchy = function (CPData) {
+			var outData = [];
+			var itSubServiceData = this._ITServiceSubTowerData;
+			var itServiceData = this._ITServiceData;
+			itSubServiceData.forEach(function (e) {
+				var subTower = {
+					"level": "IT Sub Tower",
+					"name": e["ITSubTower"],
+					"root": false,
+					"leaf": false,
+					"id": "",
+					"nodeType": "value",
+					"value": 0,
+					"parentValue": e["ITTower"],
+					"childSum": 0,
+					"child": []
+				};
+				itServiceData.forEach(function (f) {
+					if (f["ITSubTower"] === e["ITSubTower"]) {
+						subTower.child.push({
+							"level": "IT Service",
+							"name": f["ITService"],
+							"root": false,
+							"id": f["ITServiceID"],
+							"leaf": true,
+							"nodeType": "value",
+							"value": 0
+						});
+					}
+				}.bind(this));
+				outData.push(subTower);
+			});
+			return outData;
+		};
+		this._buildITT2ITSHierarchy = function (ITST2ITS) {
+			var outData = [];
+			var towerData = this._ITServiceTowerData;
+			towerData.forEach(function (e) {
+				var itTower = {
+					"level": "IT Tower",
+					"name": e["ITTower"],
+					"root": false,
+					"nodeType": "value",
+					"id": "",
+					"leaf": false,
+					"value": 0,
+					"childSum": 0,
+					"child": []
+				};
+				ITST2ITS.forEach(function (f) {
+					if (f["parentValue"] === e["ITTower"]) {
+						itTower.child.push(f);
+					}
+				}.bind(this));
+				outData.push(itTower);
+			}.bind(this));
+			return outData;
+		};
+	};
+
+	var CPIT = BaseObject.extend("pinaki.ey.CIO.allocation.api.CPIT", {
+		constructor: dataModel
+	});
+	return CPIT;
 });
--- webapp/controller/CreateAllocationDetail.controller.js
+++ webapp/controller/CreateAllocationDetail.controller.js
@@ -2,10 +2,10 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"pinaki/ey/CIO/allocation/CIOAllocation/util/Constants",
 	"pinaki/ey/CIO/allocation/CIOAllocation/api/CPIT"
-], function (Controller, Constants,CPIT) {
+], function (Controller, Constants, CPIT) {
 	"use strict";
 	var routeData = {
-		id : ''
+		id: ''
 	};
 	return Controller.extend("pinaki.ey.CIO.allocation.CIOAllocation.controller.CreateAllocationDetail", {
 		onInit: function () {
@@ -14,26 +14,123 @@ sap.ui.define([
 		},
 		_onRouteMatched: function (oEvent) {
 			routeData.id = oEvent.getParameter("arguments").id;
+			this.getView().getModel().setData({
+				allocationData: {
+					CPIT: null,
+					currentPathDesc: ''
+				}
+			}, true);
 			this.setData();
 		},
 		setData: function () {
 			this.loadApi(this.getView().getModel());
-			var aData = this.loadMockData();
-			aData.forEach(function (e, index) {
-				if (e.mapping === routeData.id) {
-					this.getView().bindElement('/allocationData/' + index+"/");
-				}
-			}.bind(this))
 		},
-		loadMockData: function () {
-			var data = [{"mapping":"CPIT","description":"Allocate Cost Center to IT Services","child":[{"level":"Cost Pool","name":"Internal Labour","value":1000,"root":true,"leaf":false,"childSum":1140,"nodeType":"display","child":[{"level":"Cost Center","name":"IT Operations","root":false,"leaf":false,"nodeType":"display","value":400,"childSum":230,"child":[{"level":"IT Tower","name":"Server","root":false,"nodeType":"value","leaf":false,"value":300,"childSum":430,"child":[{"level":"IT Sub Tower","name":"Server Internal","root":false,"leaf":false,"nodeType":"value","value":150,"childSum":230,"child":[{"level":"IT Service","name":"Exp Maintenance","root":false,"leaf":true,"nodeType":"value","value":100}]}]}]}]}]}];
-			this.getView().getModel().setProperty('/allocationData', data);
-			return data;
-		},
-		loadApi : function(){
-			if(routeData.id === 'CPIT'){
+		loadApi: function () {
+			if (routeData.id === 'CPIT') {
+				this.getView().bindElement('/allocationData/CPIT');
 				var CPITDataModel = new CPIT();
-				CPITDataModel.loadData();
+				CPITDataModel.loadInitialData().then(function (data) {
+					this.getView().getModel().setProperty('/allocationData/CPIT', data);
+					this.getView().getModel().setProperty('/allocationData/CPIT/currentPathDesc', 'Map Cost Pool to IT Services');
+				}.bind(this));
+			}
+		},
+		openCostPoolSelection: function (oEvent) {
+			var that = this;
+			var source = oEvent.getSource();
+			var path = source.getBindingContext().sPath;
+			var model = source.getModel();
+			var parentPath = path.substring(0, path.lastIndexOf('/'));
+			var cpPopover = new sap.m.Popover({
+				showHeader: false,
+				content: new sap.m.List({
+					items: {
+						path: '',
+						template: new sap.m.StandardListItem({
+							title: "{name}",
+							type: "Active",
+							press: function (oEvent) {
+								var event = oEvent;
+								oEvent.getSource().getParent().getParent().close();
+								that.changeCostPool(event);
+							}
+						})
+					}
+				})
+			});
+			cpPopover.bindElement(parentPath);
+			cpPopover.setModel(model);
+			cpPopover.openBy(source);
+		},
+		changeCostPool: function (oEvent) {
+			var path = oEvent.getSource().getBindingContext().sPath;
+			var newContext = path.substring(path.lastIndexOf('/') + 1, path.length) + '/';
+			this.getView().byId("idObjectHeader").bindElement(newContext);
+			this.getView().byId("idParentSplitter").bindElement(newContext);
+		},
+		listSelectSetBinding: function (oEvent) {
+			var selectedItem = oEvent.getParameter('listItem');
+			var path = selectedItem.getBindingContext().sPath;
+			var newContext = path.substring(path.indexOf('child'), path.length) + '/';
+
+			var list = oEvent.getSource();
+			var panel = list.getParent();
+			var splitter = panel.getParent();
+
+			var panelIndex = splitter.indexOfContentArea(panel);
+			var contents = splitter.getContentAreas();
+
+			var child = contents[panelIndex + 1];
+			child.bindElement(newContext);
+
+			// SetVisibility
+			contents.forEach(function (e) {
+				e.setVisible(true);
+			});
+			var isSelected = 'X';
+			for (var i = 0; i < contents.length; i++) {
+				var list = contents[i].getContent()[0];
+				if (isSelected !== 'X') {
+					contents[i].setVisible(false);
+				} else if (list.getSelectedItem() === null) {
+					isSelected = ' ';
+				}
+			}
+
+		},
+		onValueChange: function (oEvent) {
+			var changedValue = oEvent.getParameter('value');
+			var path = oEvent.getSource().getBindingContext().sPath;
+			var reachedTop = false;
+			var model = this.getView().getModel();
+
+			for (var i = 0; !reachedTop; i++) {
+				//GetPeers
+				var aContexts = path.split('/');
+				aContexts.pop();
+				path = aContexts.join('/');
+				var peerData = model.getProperty(path);
+				var sum = 0;
+				peerData.forEach(function (e) {
+					sum = sum + parseFloat(e.childSum);
+				});
+
+				//Get Parent
+				aContexts.pop();
+				path = aContexts.join('/');
+
+				var parentObject = model.getProperty(path);
+				if (parentObject.childSum !== undefined) {
+
+					if (sum > parentObject.value) {
+						sap.m.MessageToast.show('Error : Sum of value cannot be greater than the total allocated values');
+						parentObject["state"] = "Error";
+					} else {
+						parentObject.childSum = sum;
+					}
+				}else{
+					reachedTop = true;
+				}
 			}
 		}
 	});
